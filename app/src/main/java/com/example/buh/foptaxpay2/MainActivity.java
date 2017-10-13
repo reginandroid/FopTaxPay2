c1fc2b98e750c544abf4bc4c5e4faca9b7d4db66
reginandroid <buh.kr84@gmail.com>
2017-10-13 11:11:44 +0300

    add ContentProvider and ActionBar

--- app/src/main/java/com/example/buh/foptaxpay2/MainActivity.java
+++ app/src/main/java/com/example/buh/foptaxpay2/MainActivity.java
@@ -1,38 +1,28 @@
 package com.example.buh.foptaxpay2;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.content.Context;
 import android.content.Intent;
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.ListFragment;
-import android.support.v4.widget.SimpleCursorAdapter;
-import android.support.v7.app.ActionBarActivity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
+import android.widget.ShareActionProvider;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.AdapterView.OnItemSelectedListener;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import static android.R.attr.action;
-
-public class MainActivity extends Activity implements OnItemSelectedListener, View.OnClickListener{
-
-
+public class MainActivity extends Activity implements  OnItemSelectedListener, View.OnClickListener{
+    private ShareActionProvider mShareActionProvider;
     public static final String KEY_SPINNER_STRING = "com.example.buh.foptaxpayer.KEY_SPINNER_STRING";
-
     Button btn_edinii, btn_esv;
+    Cursor c;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,18 +36,70 @@ public class MainActivity extends Activity implements OnItemSelectedListener, Vi
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
         btn_edinii = (Button) findViewById(R.id.btn_edinii);
-        btn_esv = (Button)findViewById(R.id.btn_esv);
+        btn_esv = (Button) findViewById(R.id.btn_esv);
         btn_esv.setOnClickListener(this);
         btn_edinii.setOnClickListener(this);
 
+        }
+    @Override
+    public void onResume(){
+        super.onResume();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    public Intent doShare() {
+        String URL = "content://com.example.buh.foptaxpay2.FOPContentProvider/requisite";
+        Uri requisite = Uri.parse(URL);
+         c = this.getContentResolver().query(requisite, null, null, null, null);
+            c.moveToFirst();
+
+
+        String sharedata ="";
+
+                    sharedata = System.getProperty("line.separator") +
+                            "ОДЕРЖУВАЧ: " + c.getString(c.getColumnIndex(Constants.RECIPIENT)) + System.getProperty("line.separator") +
+                            "ЄДРПОУ: " + c.getString(c.getColumnIndex(Constants.EDRPOU)) + System.getProperty("line.separator") +
+                            "РАХУНОК: " + c.getString(c.getColumnIndex(Constants.ACCOUNT)) + System.getProperty("line.separator") +
+                            "БАНК: " + c.getString(c.getColumnIndex(Constants.BANK)) + System.getProperty("line.separator") +
+                            "МФО: " + c.getString(c.getColumnIndex(Constants.MFO)) + System.getProperty("line.separator") +
+                            "ПРИЗНАЧЕННЯ ПЛАТЕЖУ: " + c.getString(c.getColumnIndex(Constants.DETAILS));
+                    Log.d("my", sharedata);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+       intent.putExtra(Intent.EXTRA_TEXT, sharedata);
+getContentResolver().delete(requisite,null,null);
+        return intent;
     }
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
         String item = parent.getItemAtPosition(pos).toString();
+
         Intent i = new Intent (this, RequisiteFragment.class);
         i.putExtra(KEY_SPINNER_STRING, item);
         setIntent(i);
 
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_item_share:
+                mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+                mShareActionProvider.setShareIntent(doShare());
+                return true;
+
+            case R.id.help:
+                Intent i =new Intent(this, HelpActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
@@ -74,7 +116,7 @@ public class MainActivity extends Activity implements OnItemSelectedListener, Vi
                 RequisiteFragment frg_ed = new RequisiteFragment();
                android.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
                 ft.replace(R.id.frg1, frg_ed);
-              ft.addToBackStack(null);
+
 
 
                 ft.commit();
@@ -83,13 +125,21 @@ public class MainActivity extends Activity implements OnItemSelectedListener, Vi
                 ESV_Fragment frg_esv = new ESV_Fragment();
                 android.app.FragmentTransaction ft1 = getFragmentManager().beginTransaction();
                 ft1.replace(R.id.frg2, frg_esv);
-                ft1.addToBackStack(null);
+
                 ft1.commit();
                 break;
             default:
                 break;
 
         }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
 
     }
 }
